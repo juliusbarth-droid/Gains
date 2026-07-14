d6c9a90ef48ecf99a664479170840517e09c5720
root <root@srv1588139.hstgr.cloud>
2026-07-14 06:40:35 +0000

    release: clear run cues on save

--- Gains/Gains/RunTrackerView.swift
+++ Gains/Gains/RunTrackerView.swift
@@ -144,6 +144,8 @@ struct RunTrackerView: View {
             suppressNextAutoPauseSync = false
             cancelCountdown()
             countdownValue = 3
+            lastSpokenKilometer = 0
+            lastSpokenStepIndex = -1
             finishRun(title: title, note: note, feel: feel)
             phase = .setup
             showsStopSheet = false
