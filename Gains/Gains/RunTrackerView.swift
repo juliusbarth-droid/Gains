98368837d3ab7ce08258dbb7769c5abc7a36afce
root <root@srv1588139.hstgr.cloud>
2026-07-14 23:09:46 +0000

    release: dedupe run sync startup

--- Gains/Gains/RunTrackerView.swift
+++ Gains/Gains/RunTrackerView.swift
@@ -278,10 +278,6 @@ struct RunTrackerView: View {
       }
       guard !showsStopSheet, !isConfirmingCountdownAbort else { return }
       if store.activeRun != nil {
-        if store.activeRun?.modality.requiresGPS == true {
-          gpsTracker.requestAuthorization()
-        }
-        HealthKitManager.shared.startHeartRateObserver()
         phase = .live
         synchronizeTrackerState()
       } else {
