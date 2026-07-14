cdd326853f4dbae877d473c20b753a342bf04712
root <root@srv1588139.hstgr.cloud>
2026-07-14 11:38:15 +0000

    release: cancel run resume fallback countdown

--- Gains/Gains/RunTrackerView.swift
+++ Gains/Gains/RunTrackerView.swift
@@ -179,6 +179,7 @@ struct RunTrackerView: View {
               synchronizeTrackerState()
             } else {
               phase = .setup
+              cancelCountdown()
               countdownValue = 3
               lastSpokenKilometer = 0
               lastSpokenStepIndex = -1
